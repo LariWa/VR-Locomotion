72adcabbec6b6a256ce4c0553e476bedc143b5f3
Andrea Maderthaner <andrea.maderthaner@re-spaces.com>
2021-12-08 23:19:26 +0100

    Collectibles

---
+++ Assets/collect.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class collect : MonoBehaviour
+{
+    List<GameObject> collectibles = new List<GameObject>();
+    int count = 0;
+    public AudioClip magic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    foreach (GameObject collectObj in GameObject.FindGameObjectsWithTag("Collect"))
+    {
+      collectObj.SetActive(false);
+      collectibles.Add(collectObj);
+    }
+
+    selectRandom();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void selectRandom()
+    {
+      if (count < 5)
+      {
+        int random = UnityEngine.Random.Range (0, collectibles.Count);
+        collectibles[random].SetActive(true);
+        collectibles.Remove(collectibles[random]);
+      } else {
+        Debug.Log("All objects found!");
+      }
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+      if (col.gameObject.CompareTag("Collect")) {
+        AudioSource.PlayClipAtPoint(magic, transform.position);
+        Destroy(col.gameObject, (float) Convert.ToDouble("0.1"));
+        count++;
+        selectRandom();
+      }
+    }
+}
