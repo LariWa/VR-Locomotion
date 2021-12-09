8936686c01679a2eb0565d002d7470544695afe7
LariWa <larissa.wagnerberger@gmail.com>
2021-12-09 11:37:07 +0100

    measure speed and distance

---
+++ Assets/Scrips/QuantitiveEvaluator.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System;
+
+public class QuantitiveEvaluator : MonoBehaviour
+{
+    Vector3 prevPos;
+    public Transform player;
+
+    List<float> speed = new List<float>();
+    List<float> distance = new List<float>();
+
+
+    private void Start()
+    {
+      
+    }
+    private void FixedUpdate()
+    {
+       //Speed     
+        speed.Add(getSpeed(player.position));
+        distance.Add(getDistance(player.position));
+        prevPos = player.position;
+
+
+    }
+
+    private float getSpeed(Vector3 currentPos)
+    {
+        float movementPerFixedUpdate = Vector3.Distance(prevPos, currentPos);
+      
+        return movementPerFixedUpdate / Time.fixedDeltaTime;
+    }
+    private float getDistance(Vector3 currentPos)
+    {
+        float movementPerFixedUpdate = Vector3.Distance(prevPos, currentPos);
+        return movementPerFixedUpdate ;
+    }
+    private void OnDestroy()
+    {
+        Save();
+    }
+
+    void Save()
+    {
+        List<List<string>> data = new List<List<string>>();
+
+        // set titles
+        List<string> dataTemp = new List<string>(new string[] {
+        "speed",
+        "distance",
+
+         });
+        data.Add(dataTemp);
+        float allSpeed = 0;
+        float allDistances = 0;
+        for (int index = 0; index < speed.Count; index++)
+        {
+            allSpeed += speed[index];
+            allDistances += distance[index];
+        }
+         dataTemp = new List<string>();
+       
+        //average speed
+        dataTemp.Add((allSpeed / speed.Count).ToString());
+        dataTemp.Add((allDistances).ToString());
+        data.Add(dataTemp);
+      
+
+        //write to file       
+        int length = data.Count;
+        string delimiter = ";";
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int index = 0; index < length; index++)
+            sb.AppendLine(string.Join(delimiter, data[index]));
+        print(Application.dataPath);
+        string filePath = Application.dataPath + "/quantitiveMetrics" + DateTime.Now.ToString("yyyyMMddHHmmssfff")+".csv";
+
+        StreamWriter outStream = File.CreateText(filePath);
+        outStream.WriteLine(sb);
+        outStream.Close();
+    }
+}
+
