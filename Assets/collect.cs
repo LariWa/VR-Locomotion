8f9d0af7d466a1be5a3bd5268ca69295722aafdd
Andrea Maderthaner <andrea.maderthaner@re-spaces.com>
2021-12-10 11:13:57 +0100

    Routes

--- Assets/collect.cs
+++ Assets/collect.cs
@@ -1,25 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class collect : MonoBehaviour
 {
-    List<GameObject> collectibles = new List<GameObject>();
-    int count = 0;
+    public Button RouteA;
+    public Button RouteB;
+    public Button test;
     public AudioClip magic;
+
+    List<GameObject> collectItemsA = new List<GameObject>();
+    List<GameObject> collectItemsB = new List<GameObject>();
+    int count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+      collectItems("Collect_A", collectItemsA);
+      collectItems("Collect_B", collectItemsB);
 
-    foreach (GameObject collectObj in GameObject.FindGameObjectsWithTag("Collect"))
-    {
-      collectObj.SetActive(false);
-      collectibles.Add(collectObj);
-    }
+      Button btnA = RouteA.GetComponent<Button>();
+      Button btnB = RouteB.GetComponent<Button>();
+      Button btnTest = test.GetComponent<Button>();
 
-    selectRandom();
+      btnA.onClick.AddListener(delegate{selectRoute(collectItemsA);});
+      btnB.onClick.AddListener(delegate{selectRoute(collectItemsB);});
+      btnTest.onClick.AddListener(hideUI);
+
     }
 
 
@@ -30,26 +39,42 @@ public class collect : MonoBehaviour
 
     }
 
-    void selectRandom()
+    void collectItems(String tag, List<GameObject> collection)
     {
-      if (count < 5)
+      foreach (GameObject collectObj in GameObject.FindGameObjectsWithTag(tag))
       {
-        int random = UnityEngine.Random.Range (0, collectibles.Count);
-        collectibles[random].SetActive(true);
-        collectibles.Remove(collectibles[random]);
-      } else {
-        Debug.Log("All objects found!");
+        collectObj.SetActive(false);
+        collection.Add(collectObj);
+      }
+    }
+
+    void selectRoute(List<GameObject> collection)
+    {
+      foreach (GameObject collectObj in collection)
+      {
+        collectObj.SetActive(true);
       }
 
+      hideUI();
+    }
+
+    void hideUI()
+    {
+      GameObject[] buttons = GameObject.FindGameObjectsWithTag("Button");
+
+      foreach(GameObject button in buttons)
+      {
+        button.SetActive(false);
+      }
     }
 
     void OnTriggerEnter(Collider col)
     {
-      if (col.gameObject.CompareTag("Collect")) {
+      Debug.Log("Hit");
+      if (col.gameObject.CompareTag("Collect_A") || col.gameObject.CompareTag("Collect_B")) {
         AudioSource.PlayClipAtPoint(magic, transform.position);
         Destroy(col.gameObject, (float) Convert.ToDouble("0.1"));
         count++;
-        selectRandom();
       }
     }
 }
